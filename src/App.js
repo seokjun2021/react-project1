c343bf6c1c10ffad92cc1d8c2495f80a29b93a4e
DESKTOP-9NO1TOG\yyh19 <tjrwns7105@naver.com>
2021-11-20 17:54:59 +0900

    첫번째 리엑트 프로젝트

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import MainGrade from "./main_grade";
+import { Route } from "react-router-dom"
+import Detail from "./detail";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppWrap className="App">
+      <Container>
+        <Route path = "/" exact>
+          <MainGrade />
+        </Route>
+        <Route path = "/detail/:Day" exact>
+          <Detail/>
+          </Route>
+      </Container>
+    </AppWrap>
+  )
 }
+
+const AppWrap = styled.div`
+background-color: #eee;
+height: 100vh;
+width: 100vw;
+display: flex;
+flex-direction: column;
+`;
+
+const Container = styled.div`
+background-color: #fff;
+width: 50vw;
+max-width: 350px;
+margin: auto;
+height: 80vh;
+padding: 16px;
+border: 1px solid #ddd;
+border-radius: 5px;
+`;
 
 export default App;
