1c4fe4350c8967333ba4d37d300f690f7a47a4eb
DESKTOP-9NO1TOG\yyh19 <tjrwns7105@naver.com>
2021-11-25 22:02:08 +0900

    Revert "첫번째 리엑트 프로젝트"
    This reverts commit c343bf6c1c10ffad92cc1d8c2495f80a29b93a4e.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,25 @@
-import React from "react";
-import styled from "styled-components";
-import MainGrade from "./main_grade";
-import { Route } from "react-router-dom"
-import Detail from "./detail";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <AppWrap className="App">
-      <Container>
-        <Route path = "/" exact>
-          <MainGrade />
-        </Route>
-        <Route path = "/detail/:Day" exact>
-          <Detail/>
-          </Route>
-      </Container>
-    </AppWrap>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
-
-const AppWrap = styled.div`
-background-color: #eee;
-height: 100vh;
-width: 100vw;
-display: flex;
-flex-direction: column;
-`;
-
-const Container = styled.div`
-background-color: #fff;
-width: 50vw;
-max-width: 350px;
-margin: auto;
-height: 80vh;
-padding: 16px;
-border: 1px solid #ddd;
-border-radius: 5px;
-`;
 
 export default App;
